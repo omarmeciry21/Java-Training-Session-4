11f8cf6d6dff26289a303e9ad810956a555eca5e
Omar <omarmeciry21@gmail.com>
2022-03-13 23:27:57 +0200

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Array
+//	    int[] arr=new int[]{1,2,3,4,5};
+//
+//        int[] arr1 =new int[5];
+//        Scanner sc= new Scanner(System.in);
+//        for (int i=0;i<=4;i++){
+//            System.out.print("Enter number "+(i+1)+": ");
+//            arr1[i] = sc.nextInt();
+//        }
+//
+//        for(int i=0;i<=4;i++){
+//            System.out.println("Element "+i+" = "+arr1[i]);
+//        }
+
+//        boolean[] booleanArray = new boolean[]{false, true, true, false, true, true, false, true, true, false, true, true, false, true, true, false, true, true, false, true, true, false, true, true, false, true, true, false, true, true, false, true, true, false};
+//
+//        for (int i=0;i<=booleanArray.length-1;i++){
+//            if(booleanArray[i]){
+//                System.out.println("Value is TRUE!!!");
+//            }
+//            else{
+//                System.out.println("Value is FALSE!");
+//            }
+//        }
+//
+//        //Foreach
+//        for (boolean b:booleanArray) {
+//            if(b){
+//                System.out.println("Value is TRUE!!!");
+//            }
+//            else{
+//                System.out.println("Value is FALSE!");
+//            }
+//        }
+        //-----------------------------------------
+
+        //Array List
+//        ArrayList<String> arrOfNames = new ArrayList<String>();
+//
+//        arrOfNames.add("Omar");
+//        arrOfNames.add("Ahmed");
+//
+//        arrOfNames.add(1,"Mohamed");
+//
+//        arrOfNames.remove(1);
+//
+//        for(int i = 0;i<=arrOfNames.size()-1;i++) {
+//            arrOfNames.set(i,"Hello "+arrOfNames.get(i));
+//            System.out.println(arrOfNames.get(i));
+//        }
+    }
+}
